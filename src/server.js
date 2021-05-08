28beefb5308327a403fcab06917eab98c44ddb65
PhRezende-eng <ph.hr.001@gmail.com>
2021-05-08 18:01:26 -0300

    criando funçao e chamando a funçao onError para tratamento de error

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
-const { normalize } = require('path');
+// const { normalize } = require('path');
 
 const app = express();
 const port = normalizePort(process.env.PORT || '3000');
@@ -20,6 +20,7 @@ const route = router.get('/', (req,res,next)=>{
 });
 
 app.use('/pagehome', route);
+server.on('error', onError)
 
 server.listen(port);
 console.log('API rodando na porta ' + port)
@@ -36,4 +37,25 @@ function normalizePort(val){
     }
 
     return false;
+}
+
+function onError(error){
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port == 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACECES' :
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
