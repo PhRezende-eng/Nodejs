124a659ecc579b0abfe66c62751f63d88f5592e7
PhRezende-eng <ph.hr.001@gmail.com>
2021-05-09 20:12:54 -0300

    CRUD REST

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req,res,next)=>{
+const getProducts = router.get('/', (req,res,next)=>{
     res.status(200).send({
         tittle: 'Node Store API',
         version: '0.0.'
     });
 });
 
-app.use('/homepage', route);
+const createProducts = router.post('/', (req,res,next)=>{
+    res.status(201).send(req.body);
+});
+
+app.use('/', getProducts);
+app.use('/products', createProducts);
+
 
 module.exports = app;
+
+
+// STATUS
+// 200 - OK
+// 201 - CREATED
+// 400 - error.BADREQUEST
+// 401 - !AUTHENTICATED
+// 403 - !ACCESS
+// 500 - INTERNAL-SERVER-ERROR
