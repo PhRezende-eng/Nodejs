f6461b06337b41451c09b111d607fb497bba6109
PhRezende-eng <ph.hr.001@gmail.com>
2021-05-09 20:40:42 -0300

    CRUD REST

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
 const getProducts = router.get('/', (req,res,next)=>{
     res.status(200).send({
         tittle: 'Node Store API',
@@ -13,13 +16,26 @@ const getProducts = router.get('/', (req,res,next)=>{
     });
 });
 
-const createProducts = router.post('/', (req,res,next)=>{
-    res.status(201).send(req.body);
+const postProducts = router.post('/', (req,res,next)=>{
+    res.status(201).json({"message":"Produto criado!"});
+});
+
+const putProducts = router.put('/:id', (req,res,next)=>{
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const deleteProduct = router.delete('', (req, res, next)=>{
+    res.status(200).json({"message":"Produto deletado"});
 });
 
 app.use('/', getProducts);
-app.use('/products', createProducts);
-
+app.use('/products', postProducts);
+app.use('/products', putProducts);
+app.use('/products', deleteProduct);
 
 module.exports = app;
 
@@ -31,3 +47,10 @@ module.exports = app;
 // 401 - !AUTHENTICATED
 // 403 - !ACCESS
 // 500 - INTERNAL-SERVER-ERROR
+
+
+//VERBO HTTP
+// GET - OBTER INFORMAÇÃO
+// POST - CRIAR INFORMAÇÃO
+// PUT - ATUALIZAR INFORMAÇÃO
+// DELETE - DELETAR INFORMAÇÃO
